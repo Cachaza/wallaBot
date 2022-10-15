559ee26012f5e2b4e2e0b636233aa48ef01c6485
David Cachaza <9005214@alumnos.ufv.es>
2022-10-15 02:54:36 +0200

    Cambio a usar Playwright, accedo a todos los links y devulvo un JSON de cada uno con la informacion principal

--- src/Main.java
+++ src/Main.java
@@ -9,26 +9,35 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        sendPost sendPost = new sendPost();
+        playwright playwright = new playwright();
 
         String url = "https://es.wallapop.com/app/search?keywords=";
         String query = "mx keys";
-        sendPost sendPost = new sendPost();
+
         URL whatismyip = new URL("http://checkip.amazonaws.com");
         BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
 
         String ip = in.readLine(); //you get the IP as a String
         System.out.println("Esta es tu ip: "+ip);
-        JsonObject respuestaJSON = sendPost.sendPost(ip);
+        //JsonObject respuestaJSON = sendPost.sendPost(ip);
 
-        String latitud = respuestaJSON.get("latt").getAsString();
-        String longitud = respuestaJSON.get("longt").getAsString();
+        String latitud = "40.41406";//respuestaJSON.get("latt").getAsString();
+        String longitud = "-3.70158";//respuestaJSON.get("longt").getAsString();
         System.out.println("Esta es tu latitud: "+latitud);
         System.out.println("Esta es tu longitud: "+longitud);
 
         System.out.println("Buscando: " + query);
-        Document document = Jsoup.connect(url + URLEncoder.encode(query, StandardCharsets.UTF_8) + "&longitude="+longitud+"&latitude="+latitud).get();
-        System.out.println("Buscando en url: "+url + URLEncoder.encode(query, StandardCharsets.UTF_8) + "&longitude="+longitud+"&latitude="+latitud);
-        System.out.println("Total elements: " + document);
+
+        String urlFinal = url + URLEncoder.encode(query, StandardCharsets.UTF_8) + "&longitude="+longitud+"&latitude="+latitud;
+        //Document document = Jsoup.connect(url + URLEncoder.encode(query, StandardCharsets.UTF_8) + "&longitude="+longitud+"&latitude="+latitud).get();
+        System.out.println("Buscando en url: "+urlFinal);
+
+        String respuesta = playwright.getWallapop(urlFinal);
+        System.out.println(respuesta);
+
+        //System.out.println(respuesta);
+        //System.out.println("Total elements: " + document);
 
 
     }
---
+++ src/playwright.java
@@ -0,0 +1,113 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.microsoft.playwright.*;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+import com.google.gson.JsonObject;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
+
+
+public class playwright {
+    public String getWallapop(String url) throws Exception {
+        try (Playwright playwright = Playwright.create()) {
+            final BrowserType chromium = playwright.chromium();
+            final Browser browser = chromium.launch(new BrowserType.LaunchOptions().setHeadless(false));
+            final Page page = browser.newPage();
+            page.navigate(url);
+
+            JsonArray arrayFinal = new JsonArray();
+            AtomicReference<JsonObject> jsonFinal = new AtomicReference<>(new JsonObject());
+
+            Locator links = (Locator) page.locator("a.ItemCardList__item.ng-star-inserted");
+
+
+
+            synchronized (page) {
+                try {
+                    page.wait(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (page.isVisible("text=Confirm suggested preferences")) {
+                page.click("text=Confirm suggested preferences");
+            }
+            page.click("text=Ver más productos");
+            if (page.isVisible("text=Confirm suggested preferences")) {
+                page.click("text=Confirm suggested preferences");
+            }
+            System.out.println("Mas productos");
+            synchronized (page) {
+                try {
+                    page.wait(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            for (int i = 0; i <= 5; i++) {
+                links.nth(i).click();
+                synchronized (page) {
+                    try {
+                        page.wait(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                browser.contexts().forEach(context -> {
+                    context.pages().forEach(page1 -> {
+                        if (page1.url().contains("item")) {
+                            System.out.println("Page "+": " + page1.url());
+                            synchronized (page) {
+                                try {
+                                    page.wait(2000);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            String html = Jsoup.parse(page1.content()).getElementsByClass("container-detail-section with-header").html();
+                            Elements titulo = Jsoup.parse(html).getElementsByClass("js__card-product-detail--title card-product-detail-title  card-product-detail-title--with-extra-info ");
+                            Elements precio = Jsoup.parse(html).getElementsByClass("card-product-price-info");
+                            Elements descripcion = Jsoup.parse(html).getElementsByClass("js__card-product-detail--description card-product-detail-description");
+                            Elements fecha = Jsoup.parse(html).getElementsByClass("card-product-detail-user-stats-published");
+                            Elements estado = Jsoup.parse(html).getElementsByClass("ExtraInfo__text");
+                            String urlElemento = page1.url();
+                            String imagen = Jsoup.parse(html).getElementsByClass("card-slider-main ").get(0).getElementsByTag("img").attr("src");
+
+
+                            Map<String, String> resultado = Map.of(
+                                    "titulo", titulo.text(),
+                                    "precio", precio.text(),
+                                    "descripcion", descripcion.text(),
+                                    "fecha", fecha.text(),
+                                    "estado", estado.text(),
+                                    "url", urlElemento,
+                                    "imagen", imagen
+                            );
+                            Gson gsonObj = new Gson();
+                            String jsonStr = gsonObj.toJson(resultado);
+                            System.out.println(jsonStr);
+
+
+
+                            //System.out.println("HTML: " + html);
+                            page1.close();
+
+                        }
+                    });
+                });
+            }
+
+            String respuesta = arrayFinal.toString();
+
+            browser.close();
+            return respuesta;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
