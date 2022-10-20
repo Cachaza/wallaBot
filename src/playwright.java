3725da824df31b3467d295b45e14ce6e7730da14
David Cachaza <9005214@alumnos.ufv.es>
2022-10-21 00:07:28 +0200

    Limpiado codigo

--- src/playwright.java
+++ src/playwright.java
@@ -1,19 +1,13 @@
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.microsoft.playwright.*;
-
 import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
-import com.google.gson.JsonObject;
-
 
 import java.sql.SQLException;
-import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.Map;
 
 
 public class playwright {
-    public String getWallapop(String url, String query, String password, String username, String databaseUrl) throws Exception {
+    public String getWallapop(String url, String query, String password, String username, String databaseUrl) {
         try (Playwright playwright = Playwright.create()) {
             database database = new database();
             System.out.println(query);
@@ -26,10 +20,7 @@ public class playwright {
             final Page page = browser.newPage();
             page.navigate(url);
 
-            JsonArray arrayFinal = new JsonArray();
-            AtomicReference<JsonObject> jsonFinal = new AtomicReference<>(new JsonObject());
-
-            Locator links = (Locator) page.locator("a.ItemCardList__item.ng-star-inserted");
+            Locator links = page.locator("a.ItemCardList__item.ng-star-inserted");
 
 
 
@@ -88,57 +79,48 @@ public class playwright {
                         }
                     }
 
-                    browser.contexts().forEach(context -> {
-                        context.pages().forEach(page1 -> {
-                            if (page1.url().contains("item")) {
-                                System.out.println("Page "+": " + page1.url());
-                                synchronized (page) {
-                                    try {
-                                        page.wait(2000);
-                                    } catch (InterruptedException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                                String html = Jsoup.parse(page1.content()).getElementsByClass("container-detail-section with-header").html();
-                                Elements titulo = Jsoup.parse(html).getElementsByClass("js__card-product-detail--title card-product-detail-title  card-product-detail-title--with-extra-info ");
-                                Elements precio = Jsoup.parse(html).getElementsByClass("card-product-price-info");
-                                Elements descripcion = Jsoup.parse(html).getElementsByClass("js__card-product-detail--description card-product-detail-description");
-                                Elements fecha = Jsoup.parse(html).getElementsByClass("card-product-detail-user-stats-published");
-                                Elements estado = Jsoup.parse(html).getElementsByClass("ExtraInfo__text");
-                                String urlElemento = page1.url();
-                                String imagen = Jsoup.parse(html).getElementsByClass("card-slider-main ").get(0).getElementsByTag("img").attr("src");
-                                String identifcaor = urlElemento.substring(urlElemento.lastIndexOf("-") + 1);
-
-
-
-
-                                Map<String, String> resultado = Map.of(
-                                        "titulo", titulo.text(),
-                                        "precio", precio.text(),
-                                        "descripcion", descripcion.text(),
-                                        "fecha", fecha.text(),
-                                        "estado", estado.text(),
-                                        "url", urlElemento,
-                                        "imagen", imagen,
-                                        "identificador", identifcaor
-                                );
+                    browser.contexts().forEach(context -> context.pages().forEach(page1 -> {
+                        if (page1.url().contains("item")) {
+                            System.out.println("Page "+": " + page1.url());
+                            synchronized (page) {
                                 try {
-                                    database.añadir(query, resultado, password, username, databaseUrl);
-                                } catch (SQLException e) {
-                                    throw new RuntimeException(e);
+                                    page.wait(2000);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
                                 }
-                                //Gson gsonObj = new Gson();
-                                //String jsonStr = gsonObj.toJson(resultado);
-                                //System.out.println(jsonStr);
-
-
-
-                                //System.out.println("HTML: " + html);
-                                page1.close();
-
                             }
-                        });
-                    });
+                            String html = Jsoup.parse(page1.content()).getElementsByClass("container-detail-section with-header").html();
+                            Elements titulo = Jsoup.parse(html).getElementsByClass("js__card-product-detail--title card-product-detail-title  card-product-detail-title--with-extra-info ");
+                            Elements precio = Jsoup.parse(html).getElementsByClass("card-product-price-info");
+                            Elements descripcion = Jsoup.parse(html).getElementsByClass("js__card-product-detail--description card-product-detail-description");
+                            Elements fecha = Jsoup.parse(html).getElementsByClass("card-product-detail-user-stats-published");
+                            Elements estado = Jsoup.parse(html).getElementsByClass("ExtraInfo__text");
+                            String urlElemento = page1.url();
+                            String imagen = Jsoup.parse(html).getElementsByClass("card-slider-main ").get(0).getElementsByTag("img").attr("src");
+                            String identifcaor = urlElemento.substring(urlElemento.lastIndexOf("-") + 1);
+
+
+
+
+                            Map<String, String> resultado = Map.of(
+                                    "titulo", titulo.text(),
+                                    "precio", precio.text(),
+                                    "descripcion", descripcion.text(),
+                                    "fecha", fecha.text(),
+                                    "estado", estado.text(),
+                                    "url", urlElemento,
+                                    "imagen", imagen,
+                                    "identificador", identifcaor
+                            );
+                            try {
+                                database.añadir(query, resultado, password, username, databaseUrl);
+                            } catch (SQLException e) {
+                                throw new RuntimeException(e);
+                            }
+                            page1.close();
+
+                        }
+                    }));
 
                 } catch (Exception e){
                     System.out.println("Error al hacer click en el producto");
@@ -148,8 +130,6 @@ public class playwright {
 
             }
 
-
-            String respuesta = arrayFinal.toString();
 
             browser.close();
             return "Todo okay";
